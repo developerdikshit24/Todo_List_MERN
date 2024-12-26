e5a0d0aa0235c0bc5902d84c953b0b50ad450380
Dikshit Jineshwar Tibole <dikshitjtibole@gmail.com>
2024-12-26 15:45:39 +0530

    Remove Unwanted Code

--- frontend/src/Components/Todo.jsx
+++ frontend/src/Components/Todo.jsx
@@ -37,7 +37,7 @@ const Todo = () => {
     }, [handelAdd, handelEdit, handelDelete])
 
     const handelCheckbox = async (e) => {
-        const response = await axios.post(`${fetchUrl}completedTask`, { completeTodoId: e.target.name })
+        await axios.post(`${fetchUrl}completedTask`, { completeTodoId: e.target.name })
     }
     const toggleFinishedTaks = async () => {
         setTaskFinished(!taskFinished)

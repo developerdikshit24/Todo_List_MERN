fd905b68906f2d3902ed2c854584edd770307f2a
Dikshit Jineshwar Tibole <dikshitjtibole@gmail.com>
2024-12-26 16:06:18 +0530

    Fix Mini Bugs

--- frontend/src/Components/Todo.jsx
+++ frontend/src/Components/Todo.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useCallback, useEffect } from 'react'
+import { React, useState, useEffect } from 'react'
 import axios from 'axios'
 import { fetchUrl } from "../constant.jsx"
 const Todo = () => {
@@ -6,12 +6,12 @@ const Todo = () => {
     const [todos, setTodos] = useState([])
     const [taskFinished, setTaskFinished] = useState(false)
 
-    const handelAdd = useCallback(async () => {
+    const handelAdd = async (e) => {
+        e.preventDefault()
         if (todo.trim() == '') return alert("Todo is empty");
         await axios.post(`${fetchUrl}addTodo`, { newTodo: todo })
         setTodo("")
-
-    }, [todo])
+    }
 
     const handelEdit = async (e) => {
         const response = await axios.post(`${fetchUrl}getTodoById`,
@@ -27,10 +27,10 @@ const Todo = () => {
         if (response.status > 400) return console.log("Something went wrong while deleting task")
     }
 
-    const getTodo = useCallback(async () => {
+    const getTodo = async () => {
         const response = await axios.post(`${fetchUrl}getTodos`)
         setTodos(response.data.data)
-    }, [handelAdd])
+    }
 
     useEffect(() => {
         getTodo()
@@ -46,19 +46,16 @@ const Todo = () => {
     const handleChange = (e) => {
         setTodo(e.target.value);
     }
-    const handelEnter = (e) => {
-        if (e.key == "Enter") {
-            handelAdd()
-        }
-    }
+
     return (
         <div className="container max-w-3xl min-h-96 m-auto px-4 h-auto relative top-36 p-5 rounded-xl bg-purple-200">
             <h1 className='text-3xl font-bold text-center '>Todo List</h1>
             <p className='font-bold text-xl'>Add Todo:</p>
-            <div className="inputbox flex gap-3 w-full px-0 py-2" >
-                <input onChange={handleChange} onKeyUpCapture={handelEnter} value={todo} type="text" className='outline-none  rounded-xl px-3 w-full' placeholder='Add Todo' />
-                <button disabled={todo.length <= 3} onClick={handelAdd} className=' disabled:bg-neutral-600 bg-purple-600 hover:bg-purple-800 active:bg-purple-950 px-1.5 py-1.5 font-bold text-white w-14 rounded-xl border-2' type="button">Add</button>
-            </div>
+            <form className="inputbox flex gap-3 w-full px-0 py-2" >
+                <input onChange={handleChange} value={todo} type="text" className='outline-none  rounded-xl px-3 w-full' placeholder='Add Todo' />
+                <button disabled={todo.length <= 3} onClick={handelAdd} className=' disabled:bg-neutral-600 bg-purple-600 hover:bg-purple-800 active:bg-purple-950 px-1.5 py-1.5 font-bold
+                 text-white w-14 rounded-xl border-2' type="submit" >Add</button>
+            </form>
             <div className="check flex gap-1">
                 <input type="checkbox" onChange={toggleFinishedTaks} checked={taskFinished} className='accent-purple-700 cursor-pointer' name="showFinished" />
                 <label htmlFor='showFinished' className='text-sm text-neutral-700'>Show Finished</label>
